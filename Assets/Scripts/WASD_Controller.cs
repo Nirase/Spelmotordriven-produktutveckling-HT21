26c7db8f5294df57a6f7f5dab4ca23b87323e062
Rydsund <v.rydsund@gmail.com>
2021-09-14 14:27:40 +0200

    Add rigidbody controller WASD
    This is a rigidbody draft.
    WIP #6

--- Assets/Scripts/WASD_Controller.cs
+++ Assets/Scripts/WASD_Controller.cs
@@ -7,12 +7,15 @@ public class WASD_Controller : MonoBehaviour
     Rigidbody rb;
     Vector3 inputs;
 
-    [SerializeField, Range(0, 15000)]
+    [SerializeField, Range(0, 500000)]
     float Force = 5000;
+    [SerializeField, Range(0, 100)]
+    float VelocityMax = 15;
 
     // Acceleration, read about it. Possibly add.
     // MaxVelocity, to cap velocity.
     // Mass 55-67kg, is the avrage weight of an adult female woman 170cm
+    // Lower Vmax and higher force for snappier movement.
 
 
     void Start()
@@ -28,10 +31,13 @@ public class WASD_Controller : MonoBehaviour
 
         if (inputs != Vector3.zero)
             transform.forward = inputs;
+
+
     }
 
     private void FixedUpdate()
     {
-        rb.AddForce(inputs * Force * Time.fixedDeltaTime);
+        if (rb.velocity.magnitude < VelocityMax)  
+            rb.AddForce(inputs * Force * Time.fixedDeltaTime, ForceMode.Force); // Possibly use impulse instead.
     }
 }
