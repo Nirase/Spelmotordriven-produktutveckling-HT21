19505b128cc2f465ed7bcce96cdcafc7efaf6bb3
jryytty <62592941+jryytty@users.noreply.github.com>
2021-09-15 15:18:04 +0200

    Add ability to pause/unpause
    Updated the script PauseMenu on canvas to freeze/unfreeze time using 'ESC' or resume button.
    WIP #20

--- Assets/PauseMenu.cs
+++ Assets/PauseMenu.cs
@@ -4,7 +4,36 @@ using UnityEngine;
 
 public class PauseMenu : MonoBehaviour
 {
+    public static bool gameIsPaused = false;
 
+    [SerializeField] GameObject pauseMenuUI;
+
+    public void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (gameIsPaused)
+                Resume();
+            else
+                Pause();
+        }
+    }
+
+    public void Resume()
+    {
+        Debug.Log("Resume");
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        gameIsPaused = false;
+    }
+
+    void Pause()
+    {
+        Debug.Log("Pause");
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        gameIsPaused = true;
+    }
 
     public void QuitGame()
     {
