1da1dac97bd61f7887dd34ed0eb774888415ea36
jryytty <62592941+jryytty@users.noreply.github.com>
2021-09-15 16:45:47 +0200

    Allow changing resolution
    Also sets resolution to current resolution

--- Assets/SettingsMenu.cs
+++ Assets/SettingsMenu.cs
@@ -2,9 +2,44 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Audio;
+using UnityEngine.UI;
+using TMPro;
 
 public class SettingsMenu : MonoBehaviour
 {
+    [SerializeField] TMP_Dropdown resolutionDropdown;
+   
+    Resolution[] resolutions;
+    private void Start()
+    {
+        AddResolutions();
+    }
+
+    private void AddResolutions()
+    {
+        resolutions = Screen.resolutions;
+
+        resolutionDropdown.ClearOptions();
+
+        List<string> options = new List<string>();
+        int currentResolutionIndex = 0;
+
+        for (int i = 0; i < resolutions.Length; i++)
+        {
+            string option = resolutions[i].width + " x " + resolutions[i].height;
+            options.Add(option);
+
+            if(resolutions[i].width == Screen.width && resolutions[i].height == Screen.height)
+            {
+                currentResolutionIndex = i;
+            }
+        }
+
+        resolutionDropdown.AddOptions(options);
+        resolutionDropdown.value = currentResolutionIndex;
+        resolutionDropdown.RefreshShownValue();
+    }
+
     [SerializeField] AudioMixer audioMixer;
     public void SetVolume(float volume)
     {
