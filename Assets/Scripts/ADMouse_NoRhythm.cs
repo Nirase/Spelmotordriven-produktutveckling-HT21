2abdae24cacac97d7f19243e9b260af4c9c726b6
Nirase <mattias.smedman@gmail.com>
2021-09-16 11:12:27 +0200

    Add ADMouse_NoRhythm controller
    Fixes #26

---
+++ Assets/Scripts/ADMouse_NoRhythm.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ADMouse_NoRhythm : MonoBehaviour
+{
+    public float Speed = 10f;
+
+    private Rigidbody body;
+
+    bool movePlayer = false;
+    KeyCode lastInput;
+    public Camera camera;
+    void Start()
+    {
+        body = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        Ray ray = camera.ScreenPointToRay(Input.mousePosition);
+        Plane plane = new Plane(Vector3.up, Vector3.zero);
+        float distance;
+        
+        if(plane.Raycast(ray, out distance)) {
+            Vector3 target=ray.GetPoint(distance);
+            Vector3 direction=target-transform.position;
+            float rotation  = Mathf.Atan2(direction.x, direction.z)*Mathf.Rad2Deg;
+            transform.rotation=Quaternion.Euler(0, rotation, 0);
+        }
+        
+        if (Input.GetKeyDown(KeyCode.A) && lastInput != KeyCode.A)
+        {
+            lastInput = KeyCode.A;
+            movePlayer = true;
+        }
+        else if (Input.GetKeyDown(KeyCode.D) && lastInput != KeyCode.D)
+        {
+            lastInput = KeyCode.D;
+            movePlayer = true;
+        }
+    }
+    
+    private void FixedUpdate()
+    {
+        if (movePlayer == false)
+            return;
+        body.velocity += transform.forward * Speed * Time.fixedDeltaTime;
+        movePlayer = false;
+    }
+}
