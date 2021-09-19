a235b6892d95c68d2c439866f9de5e8e7bf0965f
Rydsund <v.rydsund@gmail.com>
2021-09-19 10:43:30 +0200

    Add acceleration.
    Tried adding some sort of acceleration via Lerp. Not sure exactly how it works, but does alter the feel of moving.
    WIP #24

--- Assets/Scripts/Physics Character Controller/PhysicsCharacterController.cs
+++ Assets/Scripts/Physics Character Controller/PhysicsCharacterController.cs
@@ -5,24 +5,32 @@ using UnityEngine;
 public class PhysicsCharacterController : MonoBehaviour
 {
     CharacterController controller;
-    [SerializeField, Range(0, 100)] float Speed = 10;
+    [SerializeField, Range(0, 100)] float Speed = 15;
 
     // Smooth rotation.
-    float turnSmoothTime = 0.1f;
+    float turnSmoothTime = 0.08f;
     float turnSmoothVelocity;
 
     Vector3 velocity;
     Vector3 direction;
-    float myS = 0.027f; // Friktionskoefficient för stål mot is enl w
+    float myS = 0.027f; // Friktionskoefficient för stål mot is enl wikipedia
     float myK = 0.014f;
 
     // Gravity
-    bool jump;
-    float jumpHeight = 10f;
+    public float JumpHeight = 5f;
+    public float GroundDistance = 0.2f;
+    private bool isGrounded = true;
+    private bool isJumping = false;
+    public LayerMask Ground;
+    private Transform groundChecker;
+
+    // Acceleration:
+    private float accelerationValue = 0;
 
     void Start()
     {
         controller = GetComponent<CharacterController>();
+        groundChecker = transform.GetChild(0);
     }
 
     void Update()
@@ -32,18 +40,20 @@ public class PhysicsCharacterController : MonoBehaviour
         float vertical = Input.GetAxisRaw("Vertical");
         direction = new Vector3(horizontal, 0f, vertical).normalized;
 
-        // Smooth rotation:
+        // Smooth rotation : Steer according to movement
         if (direction.magnitude >= 0.1f)
         {
             float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg;
             float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
             transform.rotation = Quaternion.Euler(0f, angle, 0f);
-        }
 
-        // Todo:
-        // Add some sort of acceleration?
-        // Fix so that glide is more "free" i.e. directions doesnt seem to snap to 8-way or mostly 4-way.
-        // Fix a jump
+            // Acceleration
+         
+        }
+  
+
+        if (Input.GetKeyDown(KeyCode.Space))
+            isJumping = true;
 
     }
 
@@ -52,19 +62,28 @@ public class PhysicsCharacterController : MonoBehaviour
         // Basic movement
         if (direction.magnitude >= 0.1f)
         {
-            velocity = direction * Speed * Time.fixedDeltaTime;
+            accelerationValue = Mathf.Lerp(0, 1, 0.9f);
+            velocity = direction * Speed * Time.fixedDeltaTime * accelerationValue;
             controller.Move(velocity);
         }
 
-        // Gravity
+        // Gravity - Jumping works but is bugged...
         velocity.y += Physics.gravity.y * Time.fixedDeltaTime;
-        if (controller.isGrounded) 
+        isGrounded = Physics.CheckSphere(groundChecker.position, GroundDistance, Ground, QueryTriggerInteraction.Ignore);
+        if (isGrounded) 
             velocity.y = 0;
+        
+        if (isJumping)
+        {
+            velocity.y += Mathf.Sqrt(JumpHeight * -2 * Physics.gravity.y);
+            isJumping = false;
+            //isGrounded = false;
+        }
 
         // Gliding on ice
         if (direction.magnitude == 0 && velocity.magnitude != 0)
         {
-            velocity -= (velocity) * myK; // Slowly glide to 0, fix this..
+            velocity -= (velocity) * myK; // Current bug, direction of glide becomes 4-way if input is not given simultaneously
             if (Mathf.Approximately(velocity.magnitude, 0))
                 velocity = Vector3.zero;
 
