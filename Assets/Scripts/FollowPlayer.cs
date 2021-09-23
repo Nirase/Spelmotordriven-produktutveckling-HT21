43a50a999f18231e74a4a4b02ea2ea7396db77d6
Rydsund <v.rydsund@gmail.com>
2021-09-23 10:52:52 +0200

    Add manager for controllers.
    Added manager for controllers, also added some fixes to controllers.
    WIP #54

--- Assets/Scripts/FollowPlayer.cs
+++ Assets/Scripts/FollowPlayer.cs
@@ -5,13 +5,23 @@ using UnityEngine;
 public class FollowPlayer : MonoBehaviour
 {
     public Transform player;
+    //Camera cam;
+    //Vector3 offSetY = new Vector3(0, 15, 0);
+
     void Start()
     {
         transform.position = player.transform.position + new Vector3(0, 15, -10);
+        //cam = GetComponent<Camera>();
     }
 
     void Update()
     {
+        //Vector3 behind = player.position -player.transform.forward * 10 + offSetY;
         transform.position = player.transform.position + new Vector3(0, 15, -10);
+
+        //cam.transform.position = Vector3.Lerp(cam.transform.position, behind, 1);
+        //Vector3 fw = player.position - cam.transform.position;
+        //cam.transform.forward = fw;
+
     }
 }
