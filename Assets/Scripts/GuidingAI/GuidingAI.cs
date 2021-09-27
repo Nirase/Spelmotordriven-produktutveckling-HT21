1311f70216915a8363a1afc93b44b1824d8d92cc
Nirase <mattias.smedman@gmail.com>
2021-09-27 11:00:08 +0200

    Add Guiding AI
    Adds an AI that guides the player towards a close by point of interest.
    
    Fixes #44

---
+++ Assets/Scripts/GuidingAI/GuidingAI.cs
@@ -0,0 +1,91 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class GuidingAI : MonoBehaviour
+{
+    private NavMeshAgent agent;
+
+    [SerializeField] private Transform player;
+
+    [SerializeField] private float stoppingDistance = 20;
+    [SerializeField] private float waitMarginal = 10;
+    [SerializeField] private float waitTime = 10;
+
+    private float timeSinceLastInteraction = 0;
+    private bool foundPlayer = false;
+    private bool searchForPlayer = false;
+
+    [SerializeField] private PointOfInterest[] goals;
+    // Start is called before the first frame update
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        agent.stoppingDistance = stoppingDistance;
+
+        StartCoroutine(UpdateTime());
+        PointOfInterest.ONTriggerEnter += InteractionDone;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (((player.position - transform.position).magnitude > stoppingDistance) && searchForPlayer)
+        {
+            agent.stoppingDistance = stoppingDistance;
+            agent.destination = player.position;
+            foundPlayer = false;
+        }
+        
+        switch (foundPlayer)
+        {
+            case true:
+            {
+                var destination = Vector3.positiveInfinity;
+                foreach (var poi in goals)
+                {
+                    var temp = poi.transform.position - player.position;
+                    if (temp.magnitude < destination.magnitude && !poi.triggered)
+                        destination = poi.transform.position;
+                }
+
+                agent.stoppingDistance = 0;
+                agent.destination = destination;
+                break;
+            }
+            case false when searchForPlayer && agent.remainingDistance < stoppingDistance - waitMarginal:
+                foundPlayer = true;
+                break;
+        }
+
+        if (timeSinceLastInteraction >= waitTime && !foundPlayer)
+        {
+            agent.destination = player.position;
+            searchForPlayer = true;
+        }
+
+        if (timeSinceLastInteraction < waitTime)
+        {
+            agent.destination = Vector3.zero;
+            searchForPlayer = false;
+            foundPlayer = false;
+        }
+    }
+
+    private void InteractionDone()
+    {
+        timeSinceLastInteraction = 0;
+        foundPlayer = false;
+        searchForPlayer = false;
+    }
+
+    private IEnumerator UpdateTime()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(1);
+            timeSinceLastInteraction++;
+        }
+    }
+}
