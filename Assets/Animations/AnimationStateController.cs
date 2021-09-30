3dcf7490c24a3012d7ba119fc0fbe2859650c5aa
Viktor Rydsund <v.rydsund@gmail.com>
2021-09-30 12:32:20 +0200

    Add more directions for 2D blend spaces
    Added working blending for WASD controller, acceleration and deceleration.
    WIP #77

--- Assets/Animations/AnimationStateController.cs
+++ Assets/Animations/AnimationStateController.cs
@@ -8,7 +8,9 @@ public class AnimationStateController : MonoBehaviour
     float VelocityZ = 0;
     float VelocityX = 0;
     Rigidbody rb;
-    float acceleration = 0.5f;
+    public float acceleration = 2f;
+    public float deceleration = 2f;
+    public float maxRunVelocity = 2.0f;
 
     void Start()
     {
@@ -18,13 +20,56 @@ public class AnimationStateController : MonoBehaviour
 
     void Update()
     {
-        bool forwardPressed = Input.GetKey("w");
+    // Get input
+        bool upPressed = Input.GetKey("w");
+        bool downPressed = Input.GetKey("s");
         bool leftPressed = Input.GetKey("a");
         bool rightPressed = Input.GetKey("d");
 
-        if(forwardPressed)
-        {
+    //Acceleration
+        // up
+        if(upPressed && VelocityZ < maxRunVelocity)
             VelocityZ += Time.deltaTime * acceleration;
-        }
+        
+        // down
+        if(downPressed && VelocityZ > - maxRunVelocity)
+            VelocityZ -= Time.deltaTime * acceleration;
+
+        // Left
+        if(leftPressed && VelocityX > -maxRunVelocity)
+            VelocityX -= Time.deltaTime * acceleration;
+        
+        // Right
+        if(rightPressed && VelocityX < maxRunVelocity)
+            VelocityX += Time.deltaTime * acceleration;
+
+    // Deceleration
+        // up
+        if(!upPressed && VelocityZ > 0.0f)
+            VelocityZ -= Time.deltaTime * deceleration;
+        
+        // down
+        if(!downPressed && VelocityZ < 0.0f)
+            VelocityZ += Time.deltaTime * deceleration;
+
+        // Clamp to zero for Z axis
+        if(!upPressed && !downPressed && VelocityZ !=0 && (VelocityZ > 0.0f && VelocityZ < 0.05f))
+            VelocityZ = 0.0f;
+
+        // Left
+        if(!leftPressed && VelocityX < 0.0f)
+            VelocityX += Time.deltaTime * deceleration;
+
+        // Right
+        if(!rightPressed && VelocityX > 0.0f)
+            VelocityX -= Time.deltaTime * deceleration;
+
+        // clamp to zero for X axis
+        if(!leftPressed && !rightPressed && VelocityX != 0.0f && (VelocityX > -0.05f && VelocityX < 0.05f))
+            VelocityX = 0.0f;
+
+
+        animator.SetFloat("VelocityZ", VelocityZ);
+        animator.SetFloat("VelocityX", VelocityX);
     }
 }
