4551e16bfbdb4ff5ea26d4bca111c41de59b4346
jryytty <62592941+jryytty@users.noreply.github.com>
2021-09-30 15:06:27 +0200

    Add logic for when agent reaches it's goal
    Will stop following agent when goal is reached.
    
    Fixes #58

--- Assets/NavMeshFollowAI.cs
+++ Assets/NavMeshFollowAI.cs
@@ -7,13 +7,32 @@ public class NavMeshFollowAI : MonoBehaviour
 {
     [SerializeField] Transform player;
     NavMeshAgent agent;
+    Vector3 startPos;
+    [SerializeField] float triggerDist = 10;
+    [SerializeField] Transform goal;
+    bool isFinished;
 
     private void Awake()
     {
         agent = GetComponent<NavMeshAgent>();
+        startPos = agent.transform.position;
+        isFinished = false;
     }
     void Update()
     {
-        agent.destination = player.position;
+
+        if (Vector3.Magnitude(agent.transform.position - goal.position) <= 1)
+            isFinished = true;
+
+        if (!isFinished)
+        {
+            if (Vector3.Magnitude(agent.transform.position - player.position) < triggerDist)
+                agent.destination = player.position;
+            else
+                agent.destination = startPos;
+        }
+        else
+            agent.destination = agent.transform.position;
+        
     }
 }
