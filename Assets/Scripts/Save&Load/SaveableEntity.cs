cccb4e82e678b02abec7cf6013706c3c97203de8
jryytty <62592941+jryytty@users.noreply.github.com>
2021-10-05 16:55:16 +0200

    Add id generator for saveable objects
    WIP #101

--- Assets/Scripts/Save&Load/SaveableEntity.cs
+++ Assets/Scripts/Save&Load/SaveableEntity.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEditor;
 
-public class Saveable_Entity : MonoBehaviour
+public class SaveableEntity : MonoBehaviour
 {
     [SerializeField] private string id = string.Empty;
 
@@ -11,4 +11,29 @@ public class Saveable_Entity : MonoBehaviour
 
     [ContextMenu("Generate ID")]
     private void GenerateId() => id = System.Guid.NewGuid().ToString();
+
+    public object CaptureState()
+    {
+        Dictionary<string, object> state = new Dictionary<string, object>();
+
+        foreach (var saveable in GetComponents<ISaveable>())
+        {
+            state[saveable.GetType().ToString()] = saveable.CaptureState();
+        }
+
+        return state;
+    }
+
+    public void RestoreState(object state)
+    {
+        Dictionary<string, object> stateDictionary = (Dictionary<string, object>)state;
+
+        foreach (var saveable in GetComponents<ISaveable>())
+        {
+            string typeName = saveable.GetType().ToString();
+
+            if (stateDictionary.TryGetValue(typeName, out object value))
+                saveable.RestoreState(value);
+        }
+    }
 }
