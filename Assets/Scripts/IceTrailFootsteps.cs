59c0237249e0460f70a7126a88bed9dea4b5ec9a
Viktor Rydsund <v.rydsund@gmail.com>
2021-10-05 17:19:07 +0200

    Add color gradient possibilities
    Added a way to change the color of the ice trails via gradient. Could possibly be expanded upon to provide masking for when one foot is in the air.
    Fixes #51

--- Assets/Scripts/IceTrailFootsteps.cs
+++ Assets/Scripts/IceTrailFootsteps.cs
@@ -6,48 +6,49 @@ public class IceTrailFootsteps : MonoBehaviour
 {
    
     ParticleSystem ps;
-    private Gradient gradient = new Gradient();
-    public bool swapColors = true;
+    //private Gradient gradient = new Gradient();
+    //public bool swapColors = true;
 
     void Start()
     {
         ps = GetComponentInChildren<ParticleSystem>();
-        var main = ps.main;
+        //var main = ps.main;
+        ps.Play();
     }
 
     // Update is called once per frame
     void Update()
     {
-        var trails = ps.trails;
-         
+        // This code can be used to set specific colors to the trail in the form of a gradient.
+        // Might be possible to do a hard transition between black and white.
 
-        if(swapColors)
-        {
-                gradient.SetKeys(
-                new GradientColorKey[] { new GradientColorKey(Color.white, 0.0f), new GradientColorKey(Color.blue, 1.0f) },
-                new GradientAlphaKey[] { new GradientAlphaKey(1.0f, 0.0f), new GradientAlphaKey(1.0f, 1.0f) } );
+        //var trails = ps.trails;
+        // if(swapColors)
+        // {
+        //         gradient.SetKeys(
+        //         new GradientColorKey[] { new GradientColorKey(Color.white, 0.0f), new GradientColorKey(Color.blue, 1.0f) },
+        //         new GradientAlphaKey[] { new GradientAlphaKey(1.0f, 0.0f), new GradientAlphaKey(1.0f, 1.0f) } );
 
-        }
-        else
-        {
-                gradient.SetKeys(
-                new GradientColorKey[] { new GradientColorKey(Color.black, 0.0f), new GradientColorKey(Color.blue, 1.0f) },
-                new GradientAlphaKey[] { new GradientAlphaKey(0.0f, 0.0f), new GradientAlphaKey(0.0f, 0.0f) } );
-        }
-
-
-        trails.colorOverLifetime = gradient;
+        // }
+        // else
+        // {
+        //         gradient.SetKeys(
+        //         new GradientColorKey[] { new GradientColorKey(Color.black, 0.0f), new GradientColorKey(Color.blue, 1.0f) },
+        //         new GradientAlphaKey[] { new GradientAlphaKey(0.0f, 0.0f), new GradientAlphaKey(0.0f, 0.0f) } );
+        // }
+        // trails.colorOverLifetime = gradient;
 
     }
     
     private void OnTriggerEnter(Collider other) 
     {
-        swapColors = true;
+        //swapColors = true;
 
     }
     private void OnTriggerExit(Collider other) 
     {
-        swapColors = false;
+     
+        //swapColors = false;
     }
 
 
