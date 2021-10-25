0fc7bcb23ea1f6c8715ad35244d0b0ec350d677d
Viktor Rydsund <v.rydsund@gmail.com>
2021-10-25 17:04:34 +0200

    Add prefab of refined controller.
    Ad Lean controller now works in a satisfactory way. There are some minor adjustments still to be made. However pushing to main so we can continue on with animations.
    Fixes #106

--- Assets/Scripts/AD Lean Controller Scripts/ADLean_Rigidbody_Controller.cs
+++ Assets/Scripts/AD Lean Controller Scripts/ADLean_Rigidbody_Controller.cs
@@ -10,23 +10,27 @@ public class ADLean_Rigidbody_Controller : MonoBehaviour
     // Not rhythm based.
 
     Rigidbody _rigidbody;
-    private Vector3 _direction;
 
+    public float displaySpeed;
     [SerializeField] float Thrust = 100f;
+    [SerializeField] float ThrustDecay = 0.985f;
     [SerializeField] float VelocityMax = 10f;
-    public Camera _camera;
 
-    float turnSmoothVelocity;
-    bool _movePlayer;
     KeyCode _lastInput;
-
-    float _targetAngle;
+    public Camera _camera;
+    private Vector3 _direction;
+    private Vector3 target = Vector3.zero;
+    private float _thrust;
+    float turnSmoothVelocity; // This is set via angular velocity.
+    [SerializeField, Range(0, 5)] float Timer = 2;
 
     private void Awake() => _rigidbody = GetComponent<Rigidbody>();
 
     void Start()
     {
         Thrust = _rigidbody.mass * Thrust;
+        _thrust = Thrust;
+        Thrust = 0; // So we dont start the game moving.
     }
 
     void Update()
@@ -37,34 +41,42 @@ public class ADLean_Rigidbody_Controller : MonoBehaviour
         float distance;
         
         if(plane.Raycast(ray, out distance)) {
-            Vector3 target = ray.GetPoint(distance);
+            
+            target = ray.GetPoint(distance);
             _direction = target - transform.position;
             _direction.Normalize();
         }
 
-        // Add timer so input isnt locked.
+        // This resets the input requirement.
+        // Possibly add time since last input to prevent spamming AD.
+        Timer -= Time.deltaTime;
+        if(Timer <= 0)
+        {
+            Timer = 2f;
+            _lastInput = KeyCode.T;
+        }
+
         if (Input.GetKeyDown(KeyCode.A) && _lastInput != KeyCode.A)
         {
             _lastInput = KeyCode.A;
-            _movePlayer = true;
+            Thrust = _thrust;
         }
         else if (Input.GetKeyDown(KeyCode.D) && _lastInput != KeyCode.D)
         {
             _lastInput = KeyCode.D;
-            _movePlayer = true;
+            Thrust = _thrust;
         }
     }
 
     public void Move()
     {   
-        if (_movePlayer == false)
-            return;
-
-
+        // If we reach maxvel mass no longer counts, right now we arrive at maxvel to quickly, therefore add more noticable acceleration.
+        Thrust = Thrust * ThrustDecay;
         _rigidbody.AddForce(_direction * Thrust * Time.fixedDeltaTime, ForceMode.Force);
-        _rigidbody.velocity = Vector3.ClampMagnitude(_rigidbody.velocity, VelocityMax);
-        
-        //_movePlayer = false;
+        _rigidbody.velocity = Vector3.ClampMagnitude(_rigidbody.velocity, VelocityMax); 
+     
+        // For debugging.
+        displaySpeed = _rigidbody.velocity.magnitude;
     }
 
     public void Rotate()
