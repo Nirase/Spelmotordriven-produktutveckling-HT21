bc52de546684ebac7bf6a09416c3f8dcd1c12fa0
IsakN <isaq.tokyo@gmail.com>
2021-11-08 10:06:02 +0100

    Add spline walker and decorator
    WIP #116

---
+++ Assets/Scripts/SplineWalker.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum SplineWalkerMode
+{
+    Once,
+    Loop
+}
+public class SplineWalker : MonoBehaviour
+{
+    [SerializeField]
+    BezierSpline spline;
+    [SerializeField, Range(0.1f, 10f)]
+    float duration;
+    [SerializeField]
+    bool lookForward;
+    [SerializeField]
+    SplineWalkerMode mode;
+    [SerializeField]
+    float offset;
+
+
+    float progress;
+    void Update()
+    {
+        progress += Time.deltaTime / duration;
+
+        if(progress > 1f)
+        {
+            if(mode == SplineWalkerMode.Once)
+            {
+                progress = 1f;
+            }
+            else if (mode == SplineWalkerMode.Loop)
+            {
+                progress -= 1f;
+            }
+        }
+        Vector3 position = spline.GetPoint(progress);
+        position.y += offset;
+        transform.localPosition = position;
+        if (lookForward)
+        {
+            transform.LookAt(position + spline.GetDirection(progress));
+        }
+    }
+}
