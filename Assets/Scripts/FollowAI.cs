25af594ae846535cf8fd261c12635631c5749144
Viktor Rydsund <v.rydsund@gmail.com>
2021-11-11 10:24:41 +0100

    Refactor code and changes to FollowAI
    WIP #131

--- Assets/Scripts/FollowAI.cs
+++ Assets/Scripts/FollowAI.cs
@@ -9,15 +9,16 @@ public class FollowAI : MonoBehaviour
     [SerializeField] Transform goal;
     [SerializeField] Transform start;
 
-    [SerializeField] int detectionDistance = 20;
-    
-
+    [SerializeField, Range(20, 100)] int detectionDistance = 50;
     private NavMeshAgent agent;
+    bool following = false;
+
     void Start()
     {
         agent = GetComponent<NavMeshAgent>();
         transform.position = start.position;
-        agent.speed = 5;
+        agent.speed = 5f;
+        agent.stoppingDistance = 10f;
     }
 
     void Update()
@@ -25,20 +26,28 @@ public class FollowAI : MonoBehaviour
         Vector3 playerPos = new Vector3(player.position.x, transform.position.y, player.position.z);
         Vector3 startToPlayer = playerPos - start.position;
 
-        if (startToPlayer.magnitude <= detectionDistance)
+
+        // Follow player
+        if (startToPlayer.magnitude <= detectionDistance && !following)
+        {
             agent.destination = playerPos;
-        else
-            agent.destination = start.position;
+            following = true;
+        }
 
-        
+        // If too far away from player, stay where you are. Possibly check for LOS?
+        Vector3 fishToPlayer = playerPos - transform.position;
+        if(fishToPlayer.magnitude > detectionDistance && following)
+        {
+            following = false;
+            agent.destination = transform.position;
+        }
+
+        // Move to goal
         Vector3 fishToGoal = goal.transform.position - transform.position;
-        
         if (fishToGoal.magnitude <= detectionDistance)
+        {
             agent.destination = goal.position;
-
-
-
-
+        }
 
     }
 }
