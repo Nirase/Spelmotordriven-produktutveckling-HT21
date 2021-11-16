7f9b6e2f4615178c9f209f6ef17cb4c4ea54b26c
Nirase <mattias.smedman@gmail.com>
2021-11-16 15:33:25 +0100

    Add wind rotate around origin

--- Assets/Scripts/Wind.cs
+++ Assets/Scripts/Wind.cs
@@ -14,25 +14,39 @@ public class Wind : MonoBehaviour
     }
 
     List<Collider> colliders = new List<Collider>();
+
+    [SerializeField] private bool useOrigin;
+    [SerializeField] private Transform origin;
+    
     [SerializeField] private Direction windDirection;
 
     [SerializeField] private float speed = 100;
     // Start is called before the first frame update
     void Start()
     {
-        switch (windDirection)
+        if (useOrigin)
         {
-            case Direction.UP:
-                break;
-            case Direction.RIGHT:
-                transform.Rotate(new Vector3(0, 1,0), 90);
-                break;
-            case Direction.DOWN:
-                transform.Rotate(new Vector3(0, 1,0), 180);
-                break;
-            case Direction.LEFT:
-                transform.Rotate(new Vector3(0, 1,0), 270);
-                break;
+            var directionVector = transform.position - origin.position;
+            directionVector.Normalize();
+
+            transform.forward = directionVector;
+        }
+        else
+        {
+            switch (windDirection)
+            {
+                case Direction.UP:
+                    break;
+                case Direction.RIGHT:
+                    transform.Rotate(new Vector3(0, 1,0), 90);
+                    break;
+                case Direction.DOWN:
+                    transform.Rotate(new Vector3(0, 1,0), 180);
+                    break;
+                case Direction.LEFT:
+                    transform.Rotate(new Vector3(0, 1,0), 270);
+                    break;
+            }
         }
         
     }
@@ -44,6 +58,12 @@ public class Wind : MonoBehaviour
         {
             //col.transform.position += transform.forward * (speed * Time.deltaTime);
             col.GetComponent<Rigidbody>().AddForce(transform.forward * (speed * Time.deltaTime), ForceMode.Force);
+            
+            if(Vector3.Dot(transform.forward, col.transform.forward) < 0)
+                col.GetComponent<ADLean_Rigidbody_Controller>().wind = true;
+            else
+                col.GetComponent<ADLean_Rigidbody_Controller>().wind = false;
+
         }
     }
 
@@ -53,7 +73,6 @@ public class Wind : MonoBehaviour
         {
             if(other.CompareTag("Player"))
             {
-                other.GetComponent<ADLean_Rigidbody_Controller>().wind = true;
                 colliders.Add(other);
             }
         }
