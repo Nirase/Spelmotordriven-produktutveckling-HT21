91941c3440068e9310918776f4b686a7b774a481
Viktor Rydsund <v.rydsund@gmail.com>
2021-11-19 12:01:43 +0100

    Add more to lighthouse puzzle
    Added code for completing puzzle, lighthouse now rises and rotates. Also moved some scrips around.
    WIP #147

---
+++ Assets/Scripts/Lighthouse Scripts/TriggerObject.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerObject : MonoBehaviour
+{
+    LighthousePuzzle _parent;
+
+    void Start() {
+        _parent = GetComponentInParent<LighthousePuzzle>();
+    }
+
+    private void OnTriggerEnter(Collider other) {
+        if(other.gameObject.tag == "Player")
+        {
+            _parent.Add();
+            gameObject.SetActive(false);
+        }
+    }
+}
