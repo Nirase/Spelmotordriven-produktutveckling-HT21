41da9162fbdd219355f2990bf679acbd8f7754ff
IsakN <isaq.tokyo@gmail.com>
2021-11-23 14:15:00 +0100

    Move Boids script from preproduction branch
    WIP #155

---
+++ Assets/Scripts/Boids/Flock.cs
@@ -0,0 +1,73 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Flock : MonoBehaviour
+{
+    [Header("Spawn Setup")]
+    [SerializeField] FlockUnit flockUnitPrefab;
+    [SerializeField] int flockSize;
+    [SerializeField] Vector3 spawnBounds;
+
+    [Header("Speed Setup")]
+    [SerializeField] float _minSpeed;
+    [SerializeField] float _maxSpeed;
+
+    public float minSpeed { get { return _minSpeed; } }
+    public float maxSpeed { get { return _maxSpeed; } }
+
+    [Header("Detection Distances")]
+    [SerializeField, Range(0, 100)] float _cohesionDistance;
+    [SerializeField, Range(0, 100)] float _avoidanceDistance;
+    [SerializeField, Range(0, 100)] float _alignmentDistance;
+    [SerializeField, Range(0, 100)] float _boundsDistance;
+    [SerializeField, Range(0, 100)] float _obstacleDistance;
+
+    public float cohesionDistance { get { return _cohesionDistance; } }
+    public float avoidanceDistance { get { return _avoidanceDistance; } }
+    public float alignmentDistance { get { return _alignmentDistance; } }
+    public float boundsDistance { get { return _boundsDistance; } }
+    public float obstacleDistance { get { return _obstacleDistance; } }
+
+    [Header("Behavoir Weights")]
+    [SerializeField, Range(0, 100)] float _cohesionWeight;
+    [SerializeField, Range(0, 100)] float _avoidanceWeight;
+    [SerializeField, Range(0, 100)] float _alignmentWeight;
+    [SerializeField, Range(0, 100)] float _boundsWeight;
+    [SerializeField, Range(0, 100)] float _obstacleWeight;
+
+    public float cohesionWeight { get { return _cohesionWeight; } }
+    public float avoidanceWeight { get { return _avoidanceWeight; } }
+    public float alignmentWeight { get { return _alignmentWeight; } }
+    public float boundsWeight { get { return _boundsWeight; } }
+    public float obstacleWeight { get { return _obstacleWeight; } }
+
+    public FlockUnit[] allUnits { get; set; }
+    void Start()
+    {
+        GenerateUnits();
+    }
+
+    void GenerateUnits()
+    {
+        allUnits = new FlockUnit[flockSize];
+        for (int i = 0; i < flockSize; i++)
+        {
+            var randomVector = Random.insideUnitSphere;
+            randomVector = new Vector3(randomVector.x * spawnBounds.x, randomVector.y * spawnBounds.y, randomVector.z * spawnBounds.z);
+            var spawnPosition = transform.position + randomVector;
+            var rotation = Quaternion.Euler(0, Random.Range(0, 360), 0);
+            allUnits[i] = Instantiate(flockUnitPrefab, spawnPosition, rotation);
+            allUnits[i].AssignFlocked(this);
+            allUnits[i].InitializeSpeed(Random.Range(minSpeed, maxSpeed));
+        }
+    }
+
+    void Update()
+    {
+        foreach (FlockUnit unit in allUnits)
+        {
+            unit.MoveUnit();
+        }
+    }
+}
---
+++ Assets/Scripts/Boids/FlockUnit.cs
@@ -0,0 +1,220 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlockUnit : MonoBehaviour
+{
+    [SerializeField] float fOVAngle;
+    [SerializeField] float smoothDamp;
+    [SerializeField] LayerMask obstacleMask;
+    [SerializeField] Vector3[] directionsToCheckForObstacles;
+
+    List<FlockUnit> cohesionNeigbhours = new List<FlockUnit>();
+    List<FlockUnit> avoidanceNeigbhours = new List<FlockUnit>();
+    List<FlockUnit> alignmentNeigbhours = new List<FlockUnit>();
+
+    Flock assignedFlock;
+    Vector3 currentVelocity;
+    Vector3 currentObstacleAvoidanceVector;
+    float speed;
+
+    public void AssignFlocked(Flock flock)
+    {
+        assignedFlock = flock;
+    }
+
+    public void InitializeSpeed(float speed)
+    {
+        this.speed = speed;
+    }
+
+    public void MoveUnit()
+    {
+        FindNeibhours();
+        CalculateSpeed();
+
+        var cohesionVector = CalculateCohesionVector() * assignedFlock.cohesionWeight;
+        var avoidanceVector = CalculateAvoidanceVector() * assignedFlock.avoidanceWeight;
+        var alignmentVector = CalculateAlignmentVector() * assignedFlock.alignmentWeight;
+        var boundsVector = CalculateBoundsVector() * assignedFlock.boundsWeight;
+        var obstacleVector = CalculateObstacleVector() * assignedFlock.obstacleWeight;
+
+        var moveVector = cohesionVector + avoidanceVector + alignmentVector + boundsVector + obstacleVector;
+        moveVector = Vector3.SmoothDamp(transform.forward, moveVector, ref currentVelocity, smoothDamp);
+        moveVector = moveVector.normalized * speed;
+        if (moveVector == Vector3.zero)
+            moveVector = transform.forward;
+
+        transform.forward = moveVector;
+        transform.position += moveVector * Time.deltaTime;
+    }
+
+    Vector3 CalculateObstacleVector()
+    {
+        var obstacleVector = Vector3.zero;
+        RaycastHit hit;
+        if (Physics.Raycast(transform.position, transform.forward, out hit, assignedFlock.obstacleDistance, obstacleMask))
+        {
+            obstacleVector = FindBestDirectionToAvoidObstacle();
+        }
+        else
+        {
+            currentObstacleAvoidanceVector = Vector3.zero;
+        }
+        return obstacleVector;
+    }
+
+    Vector3 FindBestDirectionToAvoidObstacle()
+    {
+        if (currentObstacleAvoidanceVector != Vector3.zero)
+        {
+            RaycastHit hit;
+            if (!Physics.Raycast(transform.position, transform.forward, out hit, assignedFlock.obstacleDistance, obstacleMask))
+            {
+                return currentObstacleAvoidanceVector;
+            }
+        }
+
+        float maxDistance = int.MinValue;
+        var selectedDirection = Vector3.zero;
+        for (int i = 0; i < directionsToCheckForObstacles.Length; i++)
+        {
+            RaycastHit hit;
+            var currentDirection = transform.TransformDirection(directionsToCheckForObstacles[i].normalized);
+            if (Physics.Raycast(transform.position, currentDirection, out hit, assignedFlock.obstacleDistance, obstacleMask))
+            {
+                float currentDistance = hit.distance;
+                if (currentDistance > maxDistance)
+                {
+                    maxDistance = currentDistance;
+                    selectedDirection = currentDirection;
+                }
+            }
+            else
+            {
+                selectedDirection = currentDirection;
+                currentObstacleAvoidanceVector = currentDirection.normalized;
+                return selectedDirection.normalized;
+            }
+        }
+        return selectedDirection.normalized;
+    }
+
+    Vector3 CalculateBoundsVector()
+    {
+        var centerOffset = assignedFlock.transform.position - transform.position;
+        bool isNearCenter = (centerOffset.magnitude >= assignedFlock.boundsDistance * 0.8f);
+        return isNearCenter ? centerOffset.normalized : Vector3.zero;
+    }
+
+    Vector3 CalculateCohesionVector()
+    {
+        var cohesionVector = Vector3.zero;
+        if (cohesionNeigbhours.Count == 0)
+            return cohesionVector;
+        int neigbhoursInFOV = 0;
+        for (int i = 0; i < cohesionNeigbhours.Count; i++)
+        {
+            if (IsInFOV(cohesionNeigbhours[i].transform.position))
+            {
+                neigbhoursInFOV++;
+                cohesionVector += cohesionNeigbhours[i].transform.position;
+            }
+        }
+        if (neigbhoursInFOV == 0)
+            return cohesionVector;
+
+        cohesionVector /= neigbhoursInFOV;
+        cohesionVector -= transform.position;
+        cohesionVector = cohesionVector.normalized;
+        return cohesionVector;
+    }
+
+    Vector3 CalculateAvoidanceVector()
+    {
+        var avoidanceVector = Vector3.zero;
+        if (alignmentNeigbhours.Count == 0)
+            return avoidanceVector;
+        int neighboursInFOV = 0;
+        for (int i = 0; i < avoidanceNeigbhours.Count; i++)
+        {
+            if (IsInFOV(avoidanceNeigbhours[i].transform.position))
+            {
+                neighboursInFOV++;
+                avoidanceVector += (transform.position - avoidanceNeigbhours[i].transform.position);
+            }
+        }
+        if (neighboursInFOV == 0)
+            return avoidanceVector;
+        avoidanceVector /= neighboursInFOV;
+        avoidanceVector = avoidanceVector.normalized;
+        return avoidanceVector;
+    }
+    Vector3 CalculateAlignmentVector()
+    {
+        var alignmentVector = transform.forward;
+        if (alignmentNeigbhours.Count == 0)
+            return alignmentVector;
+        int neighboursInFOV = 0;
+        for (int i = 0; i < alignmentNeigbhours.Count; i++)
+        {
+            if (IsInFOV(alignmentNeigbhours[i].transform.position))
+            {
+                neighboursInFOV++;
+                alignmentVector += alignmentNeigbhours[i].transform.forward;
+            }
+        }
+        if (neighboursInFOV == 0)
+            return alignmentVector;
+        alignmentVector /= neighboursInFOV;
+        alignmentVector = alignmentVector.normalized;
+        return alignmentVector;
+    }
+
+    void CalculateSpeed()
+    {
+        if (cohesionNeigbhours.Count == 0)
+            return;
+
+        speed = 0;
+        for (int i = 0; i < cohesionNeigbhours.Count; i++)
+        {
+            speed += cohesionNeigbhours[i].speed;
+        }
+        speed /= cohesionNeigbhours.Count;
+        speed = Mathf.Clamp(speed, assignedFlock.minSpeed, assignedFlock.maxSpeed);
+    }
+
+    void FindNeibhours()
+    {
+        cohesionNeigbhours.Clear();
+        avoidanceNeigbhours.Clear();
+        alignmentNeigbhours.Clear();
+        var allUnits = assignedFlock.allUnits;
+        for (int i = 0; i < allUnits.Length; i++)
+        {
+            var currentUnit = allUnits[i];
+            if (currentUnit != this)
+            {
+                float currentNeibhoursDistanceSqr = Vector3.SqrMagnitude(currentUnit.transform.position - transform.position);
+                if (currentNeibhoursDistanceSqr <= assignedFlock.cohesionDistance * assignedFlock.cohesionDistance)
+                {
+                    cohesionNeigbhours.Add(currentUnit);
+                }
+                if (currentNeibhoursDistanceSqr <= assignedFlock.avoidanceDistance * assignedFlock.avoidanceDistance)
+                {
+                    avoidanceNeigbhours.Add(currentUnit);
+                }
+                if (currentNeibhoursDistanceSqr <= assignedFlock.alignmentDistance * assignedFlock.alignmentDistance)
+                {
+                    alignmentNeigbhours.Add(currentUnit);
+                }
+            }
+        }
+    }
+    bool IsInFOV(Vector3 position)
+    {
+        return Vector3.Angle(transform.forward, position - transform.position) <= fOVAngle;
+    }
+}
