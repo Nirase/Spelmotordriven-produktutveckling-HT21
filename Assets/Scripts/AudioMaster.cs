74172f0f448c3c6d60ee8d3cc97ad07ce9b29bea
Viktor Rydsund <v.rydsund@gmail.com>
2021-11-28 14:42:07 +0100

    Add audio master
    WIP #164

---
+++ Assets/Scripts/AudioMaster.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using FMODUnity;
+
+public class AudioMaster : MonoBehaviour
+{
+    // This script is for controlling the master audio.
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
