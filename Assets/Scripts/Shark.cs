f706533a4f41d9a5e0d9888632599530c5cb402d
Viktor Rydsund <v.rydsund@gmail.com>
2021-11-29 21:07:13 +0100

    Add shark splines and adjust triggers
    Sharks use splines for pathing and on trigger enter for flee
    WIP #174

--- Assets/Scripts/Shark.cs
+++ Assets/Scripts/Shark.cs
@@ -5,57 +5,10 @@ using UnityEngine.AI;
 
 public class Shark : MonoBehaviour
 {
-    [SerializeField] Transform start;
-    [SerializeField] Transform end;
-    
-    [SerializeField] FollowAI wolf;
-    [SerializeField] FollowAI goat;
-    [SerializeField] FollowAI cabbage;
-
-    Vector3 sharkToEnd;
-    Vector3 sharkToStart;
-    private NavMeshAgent agent;
-    float distance = 10f;
-    [SerializeField, Range(1, 10)] float fleeDistance = 5f;
-
-    bool reverse = false;
-
-    void Start()
-    {
-        agent = GetComponent<NavMeshAgent>();
-        transform.position = start.position;
-        //transform.RotateAround(transform.position, Vector3.up, 90);
-    }
-
-    void Update()
-    {
-        sharkToEnd = end.transform.position - transform.position;
-        sharkToStart = start.transform.position - transform.position;
-
-        if(!reverse)
-            agent.destination = end.transform.position;
-        else
-            agent.destination = start.transform.position;
-
-        if(sharkToEnd.magnitude <= distance)
-            reverse = true;
-
-        if(sharkToStart.magnitude <= distance)
-            reverse = false;        
-
-        // Cause flee if too close
-        Vector3 sharkToWolf = wolf.transform.position - transform.position;
-        Vector3 sharkToCabbage = cabbage.transform.position - transform.position;
-        Vector3 sharkToGoat = goat.transform.position - transform.position;
-        
-        if(sharkToWolf.magnitude <= fleeDistance)
-            wolf.Flee();
-
-        if(sharkToCabbage.magnitude <= fleeDistance)
-            cabbage.Flee();
-
-        if(sharkToGoat.magnitude <= fleeDistance)
-            goat.Flee();
-
+    void OnTriggerEnter(Collider other) {
+        if(other.CompareTag("Goat") || other.CompareTag("Wolf") || other.CompareTag("Cabbage"))
+        {
+            other.gameObject.GetComponent<FollowAI>().Flee();
+        }
     }
 }
