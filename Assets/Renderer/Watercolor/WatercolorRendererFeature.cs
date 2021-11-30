c11e5daf98b12c80438ca7d7309fdf724bc8673d
DannyDarwiche <darwichedanny@gmail.com>
2021-11-30 17:19:44 +0100

    Set up watercolor renderer feature
    This is absolute vodoo o_O
    
    WIP #178

---
+++ Assets/Renderer/Watercolor/WatercolorRendererFeature.cs
@@ -0,0 +1,62 @@
+using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.Rendering.Universal;
+
+public class WatercolorRendererFeature : ScriptableRendererFeature
+{
+    class CustomRenderPass : ScriptableRenderPass
+    {
+        Material material;
+        RenderTargetIdentifier source;
+        RenderTargetHandle tempTexture;
+
+        public CustomRenderPass(Material material) : base()
+        {
+            this.material = material;
+            tempTexture.Init("_TempWatercolorTexture");
+        }
+
+        public void SetSource(RenderTargetIdentifier source)
+        {
+            this.source = source;
+        }
+
+        public override void Execute(ScriptableRenderContext context, ref RenderingData renderingData)
+        {
+            CommandBuffer cmd = CommandBufferPool.Get("WatercolorRendererFeature");
+
+            RenderTextureDescriptor cameraTextureDesc = renderingData.cameraData.cameraTargetDescriptor;
+            cameraTextureDesc.depthBufferBits = 0;
+            cmd.GetTemporaryRT(tempTexture.id, cameraTextureDesc, FilterMode.Bilinear);
+
+            Blit(cmd, source, tempTexture.Identifier(), material, 0);
+            Blit(cmd, tempTexture.Identifier(), source);
+
+            context.ExecuteCommandBuffer(cmd);
+            CommandBufferPool.Release(cmd);
+        }
+
+        public override void FrameCleanup(CommandBuffer cmd)
+        {
+            cmd.ReleaseTemporaryRT(tempTexture.id);
+        }
+    }
+
+    CustomRenderPass renderPass;
+
+    public override void Create()
+    {
+        var material = new Material(Shader.Find("Shader Graphs/WatercolorGlobalEffect"));
+        this.renderPass = new CustomRenderPass(material);
+
+        renderPass.renderPassEvent = RenderPassEvent.AfterRenderingPostProcessing;
+    }
+
+    public override void AddRenderPasses(ScriptableRenderer renderer, ref RenderingData renderingData)
+    {
+        renderPass.SetSource(renderer.cameraColorTarget);
+        renderer.EnqueuePass(renderPass);
+    }
+}
+
+
