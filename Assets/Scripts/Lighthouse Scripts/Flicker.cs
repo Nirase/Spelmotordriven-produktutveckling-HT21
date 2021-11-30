08da66894779a6252e864ff8cb58aff43039eb6b
Viktor Rydsund <v.rydsund@gmail.com>
2021-11-30 18:13:04 +0100

    Add lighthouse sound lerp
    Sound in lighthouse now lerps in sync with flicker.
    WIP #180

--- Assets/Scripts/Lighthouse Scripts/Flicker.cs
+++ Assets/Scripts/Lighthouse Scripts/Flicker.cs
@@ -1,9 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using FMODUnity;
+using FMOD;
 
 public class Flicker : MonoBehaviour
 {
+
+    [SerializeField] GameObject lighthouseTheme;
+    FMODUnity.StudioEventEmitter emitter;
+    private string param = "";
+
     Light lightComp;
     float m_Value;
 
@@ -14,10 +21,16 @@ public class Flicker : MonoBehaviour
     static float t = 0.0f;
     public bool flicker = true;
 
+    private float sMin = 0.0f;
+    private float sMax = 1.0f;
+    private float sLerp = 0.0f;
+
     void Start()
     {
         lightComp = GetComponent<Light>();
         m_Value = lightComp.intensity;
+        emitter = lighthouseTheme.GetComponent<FMODUnity.StudioEventEmitter>();
+        param = emitter.Params[0].Name;
     }
 
     void Update()
@@ -25,6 +38,7 @@ public class Flicker : MonoBehaviour
             if(flicker)
             {
                 lightComp.intensity = Mathf.Lerp(minimum, maximum, t) * m_Value;
+                emitter.SetParameter(param, Mathf.Lerp(sMin, sMax, t));
                 t += 0.5f * Time.deltaTime;
 
                 if (t > 1.0f)
@@ -32,12 +46,19 @@ public class Flicker : MonoBehaviour
                     float temp = maximum;
                     maximum = minimum;
                     minimum = temp;
+
+                    // Swap sound lerp
+                    float stemp = sMax;
+                    sMax = sMin;
+                    sMin = stemp;
+
                     t = 0.0f;
                 }
             }
             else
             {
                 lightComp.intensity = m_Value;
+                emitter.SetParameter(param, 1.0f);
             }
   
     }
