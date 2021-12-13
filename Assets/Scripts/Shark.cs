87187249c87f077846ff9599c9953a229cbfac15
Viktor Rydsund <v.rydsund@gmail.com>
2021-12-13 15:36:51 +0100

    Puzzle completed feedback
    When puzzle is completed shark will turn into boids.
    WIP #203

--- Assets/Scripts/Shark.cs
+++ Assets/Scripts/Shark.cs
@@ -5,10 +5,39 @@ using UnityEngine.AI;
 
 public class Shark : MonoBehaviour
 {
+    [SerializeField] GoatWolfCabbagePuzzle puzzle;
+    [SerializeField] GameObject school;
+    [SerializeField] GameObject sharkObject;
+
+    bool spawn = false;
+    bool destroy = false;
+
+
+    private void Start() {
+    }
+  
     void OnTriggerEnter(Collider other) {
         if(other.CompareTag("Goat") || other.CompareTag("Wolf") || other.CompareTag("Cabbage"))
         {
             other.gameObject.GetComponent<FollowAI>().Flee();
         }
+    }
+
+    private void Update() {
+        if(puzzle.completed)
+        {
+            if(!destroy)
+            {
+                Destroy(sharkObject);
+                destroy = true;
+            }
+
+            if(!spawn)
+            { 
+                Instantiate(school, transform);
+                spawn = true;
+            }
+        }
+
     }
 }
