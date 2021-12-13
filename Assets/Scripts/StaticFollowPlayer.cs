f681e7b87119ca0794770d2504175977b6c818fe
Nirase <mattias.smedman@gmail.com>
2021-12-13 18:40:45 +0100

    Add cinemachine blending
    WIP #200

--- Assets/Scripts/StaticFollowPlayer.cs
+++ Assets/Scripts/StaticFollowPlayer.cs
@@ -7,10 +7,15 @@ public class StaticFollowPlayer : MonoBehaviour
     // This script is a simple static camera
 
     [SerializeField] Transform Target;
+    [SerializeField] private float x = 0;
+    [SerializeField] private float y = 15;
+    [SerializeField] private float z = -10;
+
+    
     Vector3 offSet;
     void Start()
     {
-        offSet = new Vector3(0, 15, -10);
+        offSet = new Vector3(x, y, z);
         transform.position = Target.transform.position + offSet;
     }
 
