3816600edcf14f09787470d2f7b0c78f265ab755
DannyDarwiche <darwichedanny@gmail.com>
2021-12-13 17:31:08 +0100

    Add lighthouse light cone
    Makes it easier to spot the light
    
    WIP #197

--- Assets/Scripts/Lighthouse Scripts/Flicker.cs
+++ Assets/Scripts/Lighthouse Scripts/Flicker.cs
@@ -6,60 +6,51 @@ using FMOD;
 
 public class Flicker : MonoBehaviour
 {
-
+    [Header("References")]
     [SerializeField] GameObject lighthouseTheme;
-    FMODUnity.StudioEventEmitter emitter;
-    private string param = "";
+    [SerializeField] Light spotLight;
 
-    Light lightComp;
-    float m_Value;
-
-    public float minimum = -1.0F;
-    public float maximum =  1.0F;
-
-    // starting value for the Lerp
-    static float t = 0.0f;
+    [Header("Settings")]
+    [SerializeField] float frequency = 1;
     public bool flicker = true;
 
-    private float sMin = 0.0f;
-    private float sMax = 1.0f;
-    private float sLerp = 0.0f;
+    Light lightComp;
+    float maxLightIntensity;
+    float maxLightConeOpacity;
+    Renderer renderer;
+
+    FMODUnity.StudioEventEmitter emitter;
+    string param = "";
+    float sMin = 0.0f;
+    float sMax = 1.0f;
 
     void Start()
     {
-        lightComp = GetComponent<Light>();
-        m_Value = lightComp.intensity;
+        lightComp = spotLight.GetComponent<Light>();
+        maxLightIntensity = lightComp.intensity;
+        renderer = GetComponent<Renderer>();
+        maxLightConeOpacity = renderer.material.GetFloat("_Opacity");
+
         emitter = lighthouseTheme.GetComponent<FMODUnity.StudioEventEmitter>();
         param = emitter.Params[0].Name;
     }
 
     void Update()
     {
-            if(flicker)
-            {
-                lightComp.intensity = Mathf.Lerp(minimum, maximum, t) * m_Value;
-                emitter.SetParameter(param, Mathf.Lerp(sMin, sMax, t));
-                t += 0.5f * Time.deltaTime;
+        if (flicker)
+        {
+            float strength = Mathf.Clamp(Mathf.Sin(Time.time * frequency), 0, Mathf.Infinity);
 
-                if (t > 1.0f)
-                {
-                    float temp = maximum;
-                    maximum = minimum;
-                    minimum = temp;
+            lightComp.intensity = maxLightIntensity * strength;
+            renderer.material.SetFloat("_Opacity", maxLightConeOpacity * strength);
 
-                    // Swap sound lerp
-                    float stemp = sMax;
-                    sMax = sMin;
-                    sMin = stemp;
-
-                    t = 0.0f;
-                }
-            }
-            else
-            {
-                lightComp.intensity = m_Value;
-                emitter.SetParameter(param, 1.0f);
-            }
-  
+            emitter.SetParameter(param, Mathf.Lerp(sMin, sMax, strength));
+        }
+        else
+        {
+            lightComp.intensity = maxLightIntensity;
+            renderer.material.SetFloat("_Opacity", maxLightConeOpacity);
+            emitter.SetParameter(param, 1.0f);
+        }
     }
 }
